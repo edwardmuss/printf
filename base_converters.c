39e0f13f76ae42267b28d8e90cc95573567851dd
winstoneochieng <winstone.ochieng@yahoo.com>
2021-07-27 13:38:54 +0300

    comments

--- base_converters.c
+++ base_converters.c
@@ -56,7 +56,7 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
  *                 stores the result to a buffer contained in a struct.
  * @output: A buffer_t struct containing a character array.
  * @num: An unsigned long to be converted.
- * @base: A pointer to a string containing the base to convert to.
+ * @base: A pointer to a string containing the base.
  * @flags: Flag modifiers.
  * @wid: A width modifier.
  * @prec: A precision modifier.
